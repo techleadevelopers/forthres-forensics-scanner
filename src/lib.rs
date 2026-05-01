c80b25f399ab0f135edb047ffe4e95df27f19540
devopscomputer <pauloti.engenharia@gmail.com>
2026-05-01 05:43:08 -0300

    chore: update src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,10 @@ pub mod forensics;
 pub mod orchestration;
 pub mod reporting;
 pub mod service;
+pub mod verify;
 
 pub use analysis as offensive;
 pub use core as scanner;
 pub use orchestration as load_balancer;
 pub use reporting as reporter;
+pub use verify as verify_engine;
